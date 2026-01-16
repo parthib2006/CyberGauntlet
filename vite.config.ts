7199b89a08cadaa12d9a699eb296b4237bd7ab4c
Manas Srivastava <srivastavamanas32@gmail.com>
2026-01-17 00:50:49 +0530

    bug fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Root Vite config so the React JSX transform runs when starting from project root
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    open: true,
+  },
 });
